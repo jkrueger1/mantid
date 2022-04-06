f615a74e6c2e2336fc87f7a879855a055589cf0f
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2022-04-06 11:25:08 +0100

    Refactor functions define_inputs

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/Engineering/gui/engineering_diffraction/tabs/gsas2/define_inputs.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Engineering/gui/engineering_diffraction/tabs/gsas2/define_inputs.py
@@ -62,7 +62,7 @@ d_spacing_min = 1.0
 
 
 def call_subprocess(command_string):
-    shell_output = subprocess.Popen([command_string.replace('"','\\"')],
+    shell_output = subprocess.Popen([command_string.replace('"', '\\"')],
                                     shell=True,
                                     stdin=None,
                                     stdout=subprocess.PIPE,
@@ -112,57 +112,65 @@ def find_basis_block_in_file(file_path, marker_string, start_of_value, end_of_va
     return value_string
 
 
-def read_phase_and_create_reflections(data_directory, phase_files, override_cell_lengths):
-
-    # ASSUMING phase files has only 1 entry
-    phase_filepath = os.path.join(data_directory, phase_files[0])
-
-    basis = find_basis_block_in_file(phase_filepath, "atom", "\n", "loop")
-    split_string_basis = basis.split()
+def read_basis(phase_file_path):
+    basis_string = find_basis_block_in_file(phase_file_path, "atom", "\n", "loop")
+    split_string_basis = basis_string.split()
     split_string_basis[0] = ''.join([i for i in split_string_basis[0] if not i.isdigit()])
     if len(split_string_basis[0]) == 2:
         split_string_basis[0] = ''.join([split_string_basis[0][0].upper(), split_string_basis[0][1].lower()])
-
     basis = " ".join(split_string_basis[0:6]) # assuming only one line in the basis block!!
-    print("Basis of Scatterers:\n", "-"*30, "\n", basis, "\n", "-"*30, "\n")
+    return basis
 
-    space_group = find_in_file(phase_filepath, "_symmetry_space_group_name_H-M", '"', '"', strip_separator='"')
-    # Insert "-" to make the space group work with Mantid
-    for index, character in enumerate(space_group):
+
+def read_space_group(phase_file_path):
+    space_group = find_in_file(phase_file_path, "_symmetry_space_group_name_H-M", '"', '"', strip_separator='"')
+    # Insert "-" to make the space group work
+    for character_index, character in enumerate(space_group):
         if character.isdigit():
             split_string = list(space_group)
-            split_string.insert(index, "-")
+            split_string.insert(character_index, "-")
             space_group = "".join(split_string)
-    print("Space Group: ", space_group)
+    return space_group
 
-    if override_cell_lengths:
-        cell_lengths = " ".join([str(override_cell_lengths[0]),
-                                 str(override_cell_lengths[1]),
-                                 str(override_cell_lengths[2])])
+
+def choose_cell_lengths(overriding_cell_lengths, phase_file_path):
+    if overriding_cell_lengths:
+        cell_lengths = " ".join([str(overriding_cell_lengths[0]),
+                                 str(overriding_cell_lengths[1]),
+                                 str(overriding_cell_lengths[2])])
     else:
-        cell_length_a = find_in_file(phase_filepath, '_cell_length_a', ' ', '_cell_length_b')
-        cell_length_b = find_in_file(phase_filepath, '_cell_length_b', ' ', '_cell_length_c')
-        cell_length_c = find_in_file(phase_filepath, '_cell_length_c', ' ', '_cell')
+        cell_length_a = find_in_file(phase_file_path, '_cell_length_a', ' ', '_cell_length_b')
+        cell_length_b = find_in_file(phase_file_path, '_cell_length_b', ' ', '_cell_length_c')
+        cell_length_c = find_in_file(phase_file_path, '_cell_length_c', ' ', '_cell')
         cell_lengths = " ".join([cell_length_a, cell_length_b, cell_length_c])
-    print("Cell Lengths:", cell_lengths)
+    return cell_lengths
 
+
+def create_pawley_reflections(cell_lengths, space_group, basis, dmin):
     structure = CrystalStructure(cell_lengths, space_group, basis)
     generator = ReflectionGenerator(structure)
 
-    dmin = 1.0
     hkls = generator.getUniqueHKLsUsingFilter(dmin, 3.0, ReflectionConditionFilter.StructureFactor)
     dValues = generator.getDValues(hkls)
     pg = structure.getSpaceGroup().getPointGroup()
     # Make list of tuples and sort by d-values, descending, include point group for multiplicity.
-    reflections = sorted([[list(hkl), d, len(pg.getEquivalents(hkl))] for hkl, d in zip(hkls, dValues)],
-                                    key=lambda x: x[1] - x[0][0]*1e-6, reverse=True)
-    print(type([reflections[0][0]]))
-    return reflections
+    generated_reflections = sorted([[list(hkl), d, len(pg.getEquivalents(hkl))] for hkl, d in zip(hkls, dValues)],
+                                   key=lambda x: x[1] - x[0][0]*1e-6, reverse=True)
+    print(type([generated_reflections[0][0]]))
+    return generated_reflections
 
 
-def chop_to_limits(input_array, x, xmin, xmax):
-    input_array[x <= xmin] = np.nan
-    input_array[x >= xmax] = np.nan
+def check_for_output_file(temp_save_directory, name_of_project, file_extension, file_descriptor, gsas_error_string):
+    gsas_output_filename = name_of_project + file_extension
+    if gsas_output_filename not in os.listdir(temporary_save_directory):
+        raise FileNotFoundError(f"GSAS-II call must have failed, as the output {file_descriptor} file was not found.",
+                                format_shell_output(title="Errors from GSAS-II", shell_output_string=gsas_error_string))
+    return os.path.join(temp_save_directory, gsas_output_filename)
+
+
+def chop_to_limits(input_array, x, min_x, max_x):
+    input_array[x <= min_x] = np.nan
+    input_array[x >= max_x] = np.nan
     return input_array
 
 
@@ -178,24 +186,31 @@ def read_gsas_lst_and_print_wR(result_filepath):
                     print(loop_histogram, result_string[where_loop_histogram_wR: where_loop_histogram_wR_end + 1])
 
 
-def load_and_plot_gsas_histograms(save_directory, project_name, index, x_min, x_max):
-    result_csv = save_directory + project_name + f"_{index}.csv"
+def load_gsas_histogram(save_dir, name_of_project, histogram_index, min_x, max_x):
+    result_csv = save_dir + name_of_project + f"_{histogram_index}.csv"
     my_data = np.transpose(np.genfromtxt(result_csv, delimiter=",", skip_header=39))
     # x  y_obs	weight	y_calc	y_bkg	Q
     x_values = my_data[0]
-    y_obs = chop_to_limits(np.array(my_data[1]), x_values,  x_min[index], x_max[index])
-    y_calc = chop_to_limits(np.array(my_data[3]), x_values, x_min[index], x_max[index])
+    y_obs = chop_to_limits(np.array(my_data[1]), x_values,  min_x[histogram_index], max_x[histogram_index])
+    y_calc = chop_to_limits(np.array(my_data[3]), x_values, min_x[histogram_index], max_x[histogram_index])
     y_diff = y_obs - y_calc
     y_diff -= np.max(np.ma.masked_invalid(y_diff))
-    y_bkg = chop_to_limits(np.array(my_data[4]), x_values, x_min[index], x_max[index])
+    y_bkg = chop_to_limits(np.array(my_data[4]), x_values, min_x[histogram_index], max_x[histogram_index])
     y_data = np.concatenate((y_obs, y_calc, y_diff, y_bkg))
 
-    result_reflections_txt = os.path.join(save_directory, project_name + f"_reflections_{index}.txt")
-    reflection_positions = np.loadtxt(result_reflections_txt)
+    gsas_histogram = CreateWorkspace(OutputWorkspace=f"gsas_histogram_{histogram_index}",
+                                     DataX=np.tile(my_data[0], 4), DataY=y_data, NSpec=4)
+    return gsas_histogram
 
-    gsas_histogram = CreateWorkspace(DataX=np.tile(my_data[0], 4), DataY=y_data, NSpec=4)
 
-    fig, axes = plt.subplots(num=project_name + f'_{index} GSASII Refinement', subplot_kw={'projection': 'mantid'})
+def load_gsas_reflections(save_dir, name_of_project, histogram_index):
+    result_reflections_txt = os.path.join(save_dir, name_of_project + f"_reflections_{histogram_index}.txt")
+    return np.loadtxt(result_reflections_txt)
+
+
+def plot_gsas_histogram(gsas_histogram, reflection_positions, name_of_project, histogram_index, min_x, max_x):
+    fig, axes = plt.subplots(num=name_of_project + f'_{histogram_index} GSAS-II Refinement',
+                             subplot_kw={'projection': 'mantid'})
     axes.plot(gsas_histogram, color='#1105f0', label='observed', linestyle='None', marker='+', wkspIndex=0)
     axes.plot(gsas_histogram, color='#246b01', label='calculated', wkspIndex=1)
     axes.plot(gsas_histogram, color='#09acb8', label='difference', wkspIndex=2)
@@ -204,15 +219,15 @@ def load_and_plot_gsas_histograms(save_directory, project_name, index, x_min, x_
     _, y_max = axes.get_ylim()
     axes.plot(reflection_positions, [-0.10*y_max]*len(reflection_positions),
               color='#1105f0', label='reflections', linestyle='None', marker='|', mew=1.5, ms=8)
-    axes.axvline(x_min[index], color='#246b01', linestyle='--')
-    axes.axvline(x_max[index], color='#a80000', linestyle='--')
+    axes.axvline(min_x[histogram_index], color='#246b01', linestyle='--')
+    axes.axvline(max_x[histogram_index], color='#a80000', linestyle='--')
     axes.legend(fontsize=8.0).draggable().legend
     plt.show()
 
 
 def format_shell_output(title, shell_output_string):
     double_line = "-" * (len(title)+2) + "\n" + "-" * (len(title)+2)
-    return ("\n"*3 + double_line + "\n " + title + " \n" + double_line + "\n" + shell_output_string.decode() + double_line + "\n"*3)
+    return "\n"*3 + double_line + "\n " + title + " \n" + double_line + "\n" + shell_output_string.decode() + double_line + "\n"*3
 
 
 ''' Pre exec calculations '''
@@ -225,8 +240,12 @@ make_temporary_save_directory = ("mkdir -p " + temporary_save_directory)
 out_make_temporary_save_directory, err_make_temporary_save_directory = call_subprocess(make_temporary_save_directory)
 
 if refinement_method == 'Pawley':
-    mantid_pawley_reflections = read_phase_and_create_reflections(data_directory, phase_files, override_cell_lengths)
-
+    phase_filepath = os.path.join(data_directory, phase_files[0])
+    mantid_pawley_reflections = create_pawley_reflections(cell_lengths=choose_cell_lengths(override_cell_lengths,
+                                                                                           phase_filepath),
+                                                          space_group=read_space_group(phase_filepath),
+                                                          basis=read_basis(phase_filepath),
+                                                          dmin=1.0)
 
 '''Validation'''
 number_histograms = len(data_files)
@@ -241,7 +260,7 @@ if histogram_indexing and len(data_files) > 1:
                      + f"number of data_files ({len(data_files)}) == 1")
 
 if refinement_method == 'Pawley' and not mantid_pawley_reflections:
-    raise ValueError(f"No Pawley Reflections were generated for the phases provided. Not calling GSASII.")
+    raise ValueError(f"No Pawley Reflections were generated for the phases provided. Not calling GSAS-II.")
 
 if len(instrument_files) != 1 or len(instrument_files) != number_histograms:
     raise ValueError(f'The number of instrument files ({len(instrument_files)}) must be 1 '
@@ -282,39 +301,30 @@ out_call_gsas2, err_call_gsas2 = call_subprocess(call_gsas2)
 gsas_runtime = time.time() - start
 print(format_shell_output(title="Commandline output from GSAS-II", shell_output_string=out_call_gsas2))
 
-
-gsas_project_filename = project_name + '.gpx'
-gsas_project_filepath = os.path.join(temporary_save_directory, gsas_project_filename)
-if gsas_project_filename not in os.listdir(temporary_save_directory):
-    raise FileNotFoundError("GSAS-II call must have failed, as the output project file was not found.",
-                            format_shell_output(title="Errors from GSAS-II", shell_output_string=err_call_gsas2))
-
-gsas_log_filename = project_name + '.lst'
-gsas_log_filepath = os.path.join(temporary_save_directory, gsas_log_filename)
-if gsas_log_filename not in os.listdir(temporary_save_directory):
-    raise FileNotFoundError("GSAS-II call must have failed, as the output log file was not found.",
-                            format_shell_output(title="Errors from GSAS-II", shell_output_string=err_call_gsas2))
-
+gsas_project_filepath = check_for_output_file(temporary_save_directory, project_name, ".gpx", "project file", err_call_gsas2)
+gsas_result_filepath = check_for_output_file(temporary_save_directory, project_name, ".lst", "result", err_call_gsas2)
 print(f"\nGSAS-II call complete in {gsas_runtime} seconds.\n")
-print(f"GSAS-II .lst result file found. Opening {gsas_log_filename}")
-read_gsas_lst_and_print_wR(gsas_log_filepath)
 
-for index in range(number_histograms):
-    load_and_plot_gsas_histograms(save_directory, project_name, index, x_min, x_max)
+print(f"GSAS-II .lst result file found. Opening {project_name}.lst")
+read_gsas_lst_and_print_wR(gsas_result_filepath)
+
+for index_in_histograms in range(number_histograms):
+    gsas_histogram_workspace = load_gsas_histogram(save_directory, project_name, index_in_histograms, x_min, x_max)
+    reflections = load_gsas_reflections(save_directory, project_name, index_in_histograms)
+    plot_gsas_histogram(gsas_histogram_workspace, reflections, project_name, index_in_histograms, x_min, x_max)
 
 make_user_save_directory = ("mkdir -p " + user_save_directory)
 out_make_user_save_directory, err_make_user_save_directory = call_subprocess(make_user_save_directory)
 if err_make_user_save_directory:
     raise ValueError("Could not create the user save directory {user_save_directory}")
 
-if os.listdir(temporary_save_directory): # currently just checking if there is something in the temp save directory
-    for output_file in os.listdir(temporary_save_directory):
-        os.rename(os.path.join(temporary_save_directory, output_file),
-                  os.path.join(user_save_directory, output_file))
-    os.rmdir(temporary_save_directory)
-    print(f"\n\nOutput GSAS-II files saved in {user_save_directory}")
+for output_file in os.listdir(temporary_save_directory):
+    os.rename(os.path.join(temporary_save_directory, output_file),
+              os.path.join(user_save_directory, output_file))
+os.rmdir(temporary_save_directory)
+print(f"\n\nOutput GSAS-II files saved in {user_save_directory}")
 
-# # open GSASII project
+# # open GSAS-II project
 # open_project_call = (path_to_gsas2 + "bin/python " + path_to_gsas2 + "GSASII/GSASII.py "
 #                      + os.path.join(user_save_directory, project_name + ".gpx"))
 # out_open_project_call, err_open_project_call = call_subprocess(open_project_call)
